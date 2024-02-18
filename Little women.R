d8d30699a40088a5c00f761dcc779cf194adb3ab
Yemi Ayoade <yemee.ayoade@gmail.com>
2024-02-18 16:21:08 -0700

    Initial commit

---
+++ Little women.R
@@ -0,0 +1,16 @@
+
+library(ggplot2)
+
+# create data
+Little_women <- data.frame(
+  name = c("Meg", "Jo", "Beth", "Amy") ,
+  Times.Mentioned = c(683,1355,459,645)
+)
+
+# Create plot Object
+LittleWomen_plot <- ggplot(data = Little_women, mapping = aes(x = name, y = Times.Mentioned)) +
+      geom_bar(stat = "identity")
+
+# Draw plot.
+print(LittleWomen_plot)
+
